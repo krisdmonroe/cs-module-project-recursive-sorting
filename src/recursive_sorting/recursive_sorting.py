40b118ac2ffaf2827ddfe41e8342431ba4c0ae28
Kris Monroe <krisdmonroe@gmail.com>
2020-05-20 12:32:26 -0700

    mvp redone

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,10 +1,29 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
-
+    # elements = len(arrA) + len(arrB)
+    # merged_arr = [0] * elements
+    merged_arr = []
     # Your code here
+    a = 0
+    b = 0
 
+    while a < len(arrA) and b < len(arrB):
+        # compare the elements that a and b point at 
+        if arrA[a] < arrB[b]:
+            merged_arr.append(arrA[a])
+            a += 1
+        else:
+            merged_arr.append(arrB[b])
+            b += 1
+
+    # at this point, we've finished traversing one of the lists completely
+    # we need to add all of the elements from the other list to the combined list 
+    while a < len(arrA):
+        merged_arr.append(arrA[a])
+        a += 1 
+    while b < len(arrB):
+        merged_arr.append(arrB[b])
+        b += 1
 
     return merged_arr
 
@@ -12,37 +31,42 @@ def merge(arrA, arrB):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
     # Your code here
-    if len(arr) >1: 
-        mid = len(arr)//2 #Finding the mid of the array 
-        L = arr[:mid] # Dividing the array elements  
-        R = arr[mid:] # into 2 halves 
+    # if len(arr) >1: 
+    #     mid = len(arr)//2 #Finding the mid of the array 
+    #     L = arr[:mid] # Dividing the array elements  
+    #     R = arr[mid:] # into 2 halves 
   
-        merge_sort(L) # Sorting the first half 
-        merge_sort(R) # Sorting the second half 
+    #     merge_sort(L) # Sorting the first half 
+    #     merge_sort(R) # Sorting the second half 
   
-        i = j = k = 0
+    #     i = j = k = 0
           
-        # Copy data to temp arrays L[] and R[] 
-        while i < len(L) and j < len(R): 
-            if L[i] < R[j]: 
-                arr[k] = L[i] 
-                i+=1
-            else: 
-                arr[k] = R[j] 
-                j+=1
-            k+=1
+    #     # Copy data to temp arrays L[] and R[] 
+    #     while i < len(L) and j < len(R): 
+    #         if L[i] < R[j]: 
+    #             arr[k] = L[i] 
+    #             i+=1
+    #         else: 
+    #             arr[k] = R[j] 
+    #             j+=1
+    #         k+=1
           
-        # Checking if any element was left 
-        while i < len(L): 
-            arr[k] = L[i] 
-            i+=1
-            k+=1
+    #     # Checking if any element was left 
+    #     while i < len(L): 
+    #         arr[k] = L[i] 
+    #         i+=1
+    #         k+=1
           
-        while j < len(R): 
-            arr[k] = R[j] 
-            j+=1
-            k+=1
-
+    #     while j < len(R): 
+    #         arr[k] = R[j] 
+    #         j+=1
+    #         k+=1
+    if len(arr) > 1:
+    # break the array down recursively
+    # base case: when the lists get down to lengths of 1 
+        left = merge_sort(arr[:len(arr) // 2])
+        right = merge_sort(arr[len(arr) // 2:])
+        arr = merge(left, right)
     return arr
 
 
